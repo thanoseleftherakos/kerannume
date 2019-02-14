9254e493be45a3cf41ae1b5546771cd82a095d60
Thanos Eleftherakos <hello@bracket.gr>
2019-02-14 14:31:58 +0200

    add grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 window.jQuery = $ = require('jquery');
+var Packery = require('packery');
 
 $(document).ready(function(){
 
@@ -10,4 +11,24 @@ $(document).ready(function(){
         $('.nav-menu').removeClass('show');
     })
 
+    var elem = document.querySelector('.grid');
+    var pckry = new Packery( elem, {
+        // options
+        itemSelector: '.grid__item',
+        percentPosition: true,
+      });
+      $('.grid__item').each(function(){
+        var m = Math.floor(Math.random() * 20) + 1  ;
+        var h = Math.floor(Math.random() * 20) + 1  ;
+        var width = Math.floor(Math.random() * (40 - 20 + 1)) + 20;
+        $(this).css({'max-width' :450+'px',  'margin-left' : m+'em', 'margin-top' : h+'em'});
+      })
+      pckry.layout();
+      pckry.once( 'layoutComplete', function() {
+        $('.grid').css({'opacity': 1});
+      });
 });
+
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+  }
